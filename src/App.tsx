2cb6379d84708b4e95368e8803f22669dd1f1216
Olga Greyskop <o.greyskop@gmail.com>
2023-02-12 20:30:21 +0200

    hw#40 version Yura

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import './App.css'
+import { Layout } from './components/navigators/Layout';
+import { Products } from './components/navigators/Products';
+import { BreadProducts } from './components/pages/BreadProducts';
+import { Customers } from './components/pages/Customers';
+import { DairyProducts } from './components/pages/DairyProducts';
+import { Home } from './components/pages/Home';
+import { Orders } from './components/pages/Orders';
+
+function App() {
+  return <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Layout/>}>
+              <Route index element={<Home/>}></Route>
+              <Route path='customers' element={<Customers/>}/>
+              <Route path='orders' element={<Orders/>}></Route>
+              <Route path='products' element={<Products/>}>
+                    <Route path='dairy' element={<DairyProducts/>}/>
+                    <Route path='bread' element={<BreadProducts/>}/>
+              </Route>
+          </Route>
+              
+      </Routes>
+  </BrowserRouter>
+
+}
+export default App;
